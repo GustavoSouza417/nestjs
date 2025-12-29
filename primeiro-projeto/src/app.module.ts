95e677de83edddd9616f43e45d6f182169c7a793
GustavoSouza417 <gustavosouza.pro417@gmail.com>
2025-12-28 21:19:25 -0300

    feat: módulos do sistema

--- primeiro-projeto/src/app.module.ts
+++ primeiro-projeto/src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { DatabaseModule } from './database/database.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, DatabaseModule]
+  imports: [AuthModule, UserModule, PrismaModule]
 })
 export class AppModule {}
