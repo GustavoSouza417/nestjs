271ec79b484c5324e6e16ec68c39014e9c91708c
GustavoSouza417 <gustavosouza.pro417@gmail.com>
2025-12-28 21:20:18 -0300

    feat: esboço de getUser

---
+++ primeiro-projeto/src/user/user.repository.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { user } from '@prisma/client';
+
+@Injectable()
+export class UserRepository {
+  constructor(private prisma: PrismaService) {}
+
+  public getUser(): string {
+    return "Repository User!";
+  }
+}
